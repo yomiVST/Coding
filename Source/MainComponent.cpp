ca06f3a286385812612848a692301855c81f42f4
yomi <abayomicraddock@gmail.com>
2024-10-21 13:58:24 -0400

    Changed Font Size

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -16,9 +16,9 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::FontOptions (16.0f));
+    g.setFont (juce::FontOptions (64.0f));
     g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    g.drawText ("Coding Class Test", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
